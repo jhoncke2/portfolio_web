80bcaa7ecee03bf0f64fb887c6fc51562deed777
Jhonatan Amórtegui <yonandres97@gmail.com>
2025-06-02 18:58:32 -0500

    envío de mensaje implementado

--- lib/home/domain/url_navitator.dart
+++ lib/home/domain/url_navitator.dart
@@ -21,21 +21,14 @@ class UrlNavigator{
   }
 
   Future<void> sendMessage(String recipient, String body)async{
-    final String subject = Uri.encodeComponent('Mensaje del portafolio Web');
-    final String encodedBody = Uri.encodeComponent(body);
-    final String mailtoLink = 'mailto:$recipient?subject=$subject&body=$encodedBody';
     final mailToUri = Uri(
       scheme: 'mailto',
       path: recipient,
-      query: Uri.encodeFull(
-        'subject=$subject&body=$encodedBody'
-      )
+      queryParameters: {
+        'subject': 'Mensaje del portafolio web',
+        'body': body
+      }
     );
-    print('*************\n$mailtoLink');
-    //html.AnchorElement(href: mailtoLink)
-    //  ..target = '_blank'
-    //  ..click();
-    //html.window.open(mailtoLink, '_blank');
     html.window.location.href = mailToUri.toString();
   }
 
